12b5ee2ac2489c669094ba700325f75514887931
AlfinAldy <alfin.gamedev@gmail.com>
2026-03-08 13:03:10 +0700

    update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,7 +4,6 @@ const navLinks = [
     { href: "#about", label: 'About' },
     { href: "#projects", label: 'Projects' },
     { href: "#experience", label: 'Experience' },
-    { href: "#testimonials", label: 'Testimonials' },
     { href: "#contact", label: 'Contact' },
 ];
 
